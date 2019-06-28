1a01a0b48c9cd7c356e292e2da61af2b722af53d
jlinares <jlinares0996@gmail.com>
2019-06-27 21:34:10 -0500

    Validaciones Registrar Cliente

--- resources/views/regcli.blade.php
+++ resources/views/regcli.blade.php
@@ -13,7 +13,7 @@
 
                               <div class="col-md-6">
                                   <label for="street1_id" class="control-label ">DNI / RUC</label>
-                                  <input type="text" class="form-control textform" id="street1_id" name="nrodoc"  placeholder="Numero de documento del cliente">
+                                  <input type="text" pattern="^([0-9]{0,2}){5,12}$" class="form-control textform" id="street1_id" name="nrodoc"  placeholder="Numero de documento del cliente" required>
                               </div>
 
                           </div>
@@ -22,7 +22,7 @@
 
                               <div class="col-md-6">
                                   <label for="street1_id" class="control-label">Cliente</label>
-                                  <input type="text" class="form-control textform" id="street1_id" name="nameCustomer"  placeholder="Nombres del Cliente">
+                                  <input type="text" class="form-control textform" id="street1_id" name="nameCustomer"  placeholder="Nombres del Cliente" required>
                               </div>
 
                           </div>
@@ -32,18 +32,19 @@
 
                               <div class="col-md-6">
                                   <label for="street1_id" class="control-label">Email</label>
-                                  <input type="text" class="form-control textform" id="street1_id" name="email"  placeholder="Email del cliente">
+                                  <input type="email" pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" class="form-control textform" id="street1_id" name="email"  placeholder="Email del cliente" required>
                               </div>
                               <div class="col-md-6">
                                   <label for="street1_id" class="control-label">Teléfono</label>
-                                  <input type="text" class="form-control textform" id="street1_id" name="phone"  placeholder="Telefono de referéncia cliente">
+                                  <input type="text" pattern="^([0-9]{0,2}){5,12}$" class="form-control textform" id="street1_id" name="phone"  placeholder="Telefono de referéncia cliente" required>
                               </div>
                           </div>
 
                           <div class="form-group col-md-12"> <!-- Street 1 -->
 
                             <div class="col-md-6">
-                                    <button for="" type="submit" class="btn btn-primary">Registrar Cliente</button>
+                            <label for="" class="control-label ">&nbsp;</label>
+                                    <button for="" type="submit"  class="btn btn-primary">Registrar Cliente</button>
                             </div>
 
                           </div>
